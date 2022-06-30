f02a0c4c51648a809d86876b655303ae5ce3e6f7
tolya-yanot <1449561+tolya-yanot@users.noreply.github.com>
2022-06-30 03:14:18 +0300

    dns contracts

--- func/compile.sh
+++ func/compile.sh
@@ -1,7 +1,8 @@
 rm build/nft-item-code.fif
 rm build/nft-collection-code.fif
 
-func -o build/nft-item-code.fif -SPA stdlib.fc params.fc op-codes.fc nft-item.fc
-func -o build/nft-collection-code.fif -SPA stdlib.fc params.fc op-codes.fc nft-collection.fc
+func -o build/nft-item-code.fif -SPA stdlib.fc params.fc op-codes.fc dns-utils.fc nft-item.fc
+func -o build/nft-collection-code.fif -SPA stdlib.fc params.fc op-codes.fc dns-utils.fc nft-collection.fc
+func -o build/root-dns-code.fif -SPA stdlib.fc dns-utils.fc root-dns.fc
 
 fift -s build/print-hex.fif
